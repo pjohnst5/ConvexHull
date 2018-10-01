6fefb2225de7eb012c0c47f95951acfc9dadf2a8
Paul Johnston <pjohnst5@icloud.com>
2018-10-01 15:53:38 -0600

    LEts gooo

--- convex_hull.py
+++ convex_hull.py
@@ -19,7 +19,7 @@ import time
 
 class ConvexHullSolverThread(QThread):
 	def __init__( self, unsorted_points,demo):
-		self.points = unsorted_points					
+		self.points = unsorted_points
 		self.pause = demo
 		QThread.__init__(self)
 
@@ -33,28 +33,53 @@ class ConvexHullSolverThread(QThread):
 	show_tangent = pyqtSignal(list,tuple)
 	erase_hull = pyqtSignal(list)
 	erase_tangent = pyqtSignal(list)
-					
 
-	def run( self):
+#Begin My functions
+	def slope(self, p1, p2):
+		rise = p1.y() - p2.y()
+		run = p1.x() - p2.x()
+		return rise/run
+
+	def clockwiseOrder(self, points):
+		if len(points) == 1:
+			return points, 0
+		elif len(points) == 2:
+			return points, 1
+		else:
+			slopeToB = self.slope()
+
+
+	def getHull(self, points):
+		if len(points) < 4:
+			return self.clockwiseOrder(points)
+
+#End my functions
+
+	def run(self):
 		assert( type(self.points) == list and type(self.points[0]) == QPointF )
 
 		n = len(self.points)
 		print( 'Computing Hull for set of {} points'.format(n) )
 
+		#Sorts points by increasing X value
 		t1 = time.time()
-		# TODO: SORT THE POINTS BY INCREASING X-VALUE
+		self.points.sort(key=lambda point: point.x(), reverse=False)
+
 		t2 = time.time()
 		print('Time Elapsed (Sorting): {:3.3f} sec'.format(t2-t1))
 
+		#Time to compute the hull using divide and conquer
 		t3 = time.time()
-		# TODO: COMPUTE THE CONVEX HULL USING DIVIDE AND CONQUER
+		answer = self.getHull(self.points)
+
+
 		t4 = time.time()
 
 		USE_DUMMY = True
 		if USE_DUMMY:
 			# this is a dummy polygon of the first 3 unsorted points
 			polygon = [QLineF(self.points[i],self.points[(i+1)%3]) for i in range(3)]
-			
+
 			# when passing lines to the display, pass a list of QLineF objects.  Each QLineF
 			# object can be created with two QPointF objects corresponding to the endpoints
 			assert( type(polygon) == list and type(polygon[0]) == QLineF )
@@ -64,11 +89,7 @@ class ConvexHullSolverThread(QThread):
 		else:
 			# TODO: PASS THE CONVEX HULL LINES BACK TO THE GUI FOR DISPLAY
 			pass
-			
+
 		# send a signal to the GUI thread with the time used to compute the hull
 		self.display_text.emit('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
 		print('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
-			
-
-
-
